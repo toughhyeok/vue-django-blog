76e951f9ccbc5e971049594a604e0977027b4fbb
junhyeok <toughhyeok96@gmail.com>
2022-09-02 00:50:40 +0900

    [FEAT] blog 파일 admin 등록

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
-    tags = models.ManyToManyField('Tag', blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
     description = models.CharField('DESCRIPTION', max_length=100, blank=True, help_text='simple one-line text.')
     title = models.CharField('TITLE', max_length=50)
     image = models.ImageField('IMAGE', upload_to='blog/%Y/%m/', blank=True, null=True)
