e1e1c0e0d939b8175aeaf19949e854a950c3fb7d
junhyeok <toughhyeok96@gmail.com>
2022-09-08 18:13:34 +0900

    [FEAT] order by create_dt desc

--- blog/models.py
+++ blog/models.py
@@ -3,29 +3,21 @@ from django.db import models
 
 class Post(models.Model):
     category = models.ForeignKey(
-        'Category',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True
-    )
+        'Category', on_delete=models.SET_NULL, blank=True, null=True)
     tags = models.ManyToManyField('Tag', blank=True)
     description = models.CharField(
-        'DESCRIPTION',
-        max_length=100,
-        blank=True,
-        help_text='simple one-line text.'
-    )
+        'DESCRIPTION', max_length=100, blank=True,
+        help_text='simple one-line text.')
     title = models.CharField('TITLE', max_length=50)
     image = models.ImageField(
-        'IMAGE',
-        upload_to='blog/%Y/%m/',
-        blank=True,
-        null=True
-    )
+        'IMAGE', upload_to='blog/%Y/%m/', blank=True, null=True)
     content = models.TextField('CONTENT')
     create_dt = models.DateTimeField('CREATE DT', auto_now_add=True)
     update_dt = models.DateTimeField('UPDATE DT', auto_now=True)
     like = models.PositiveSmallIntegerField('LIKE', default=0)
+
+    class Meta:
+        ordering = ('-create_dt',)
 
     def __str__(self):
         return self.title
@@ -34,11 +26,8 @@ class Post(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(
-        'DESCRIPTION',
-        max_length=100,
-        blank=True,
-        help_text='simple one-line text.'
-    )
+        'DESCRIPTION', max_length=100, blank=True,
+        help_text='simple one-line text.')
 
     def __str__(self):
         return self.name
@@ -53,11 +42,7 @@ class Tag(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True
-    )
+        Post, on_delete=models.CASCADE, blank=True, null=True)
     content = models.TextField('CONTENT')
     create_dt = models.DateTimeField('CREAT DT', auto_now_add=True)
     update_dt = models.DateTimeField('UPDATE DT', auto_now=True)
