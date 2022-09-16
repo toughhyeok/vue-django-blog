edc06f31ab8983c82cb3b16bc807abff8299f92d
junhyeok <toughhyeok96@gmail.com>
2022-09-16 22:22:56 +0900

    [ENV] git secret key

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 
+import json
+
 from pathlib import Path
+
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(c1+-2nhs(7cj)hi00pvym))usjfg06r+1vy)inzte0p3aa5rt'
+SECRET_FILE_PATH = os.path.join(BASE_DIR, 'secrets.json')
+with open(SECRET_FILE_PATH) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = ')+(KmeEY#_yk8TSBT>F0I?7%_*iJL*nF,&-YRnO*E>j*eo7eW8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
