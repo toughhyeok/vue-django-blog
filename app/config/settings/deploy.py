4f229c0d016400d481cdbd79942cc188849b94d3
junhyeok <toughhyeok96@gmail.com>
2022-09-27 22:33:40 +0900

    [FEAT] postgre -> sqlite

--- app/config/settings/deploy.py
+++ app/config/settings/deploy.py
@@ -3,16 +3,6 @@ from app.config.settings.base import *
 
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': get_secret('DB_NAME'),
-        'USER': get_secret('DB_USER'),
-        'PASSWORD': get_secret('DB_PASS'),
-        'HOST': get_secret('DB_HOST')
-    }
-}
-
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = "/var/www/share-blog/static/"
